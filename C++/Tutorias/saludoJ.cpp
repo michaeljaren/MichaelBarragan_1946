576331c01732fbd83203417dbf12b12688d377b8
michaeljaren <michaelbarragan.j.g@gmail.com>
2025-03-04 15:14:59 -0500

    saludo_Joseplhin

---
+++ C++/Tutorias/saludoJ.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+    
+    cout << " Hola como estas Josh , pues esta es la vida del mejor guerrero de Diosa" << endl;
+    cout<< " Espero que te este llendo espectacular"<<endl;
+    cout<<" No eres tan blanca solo que te agarra bien el filtro ";
+    
+}
